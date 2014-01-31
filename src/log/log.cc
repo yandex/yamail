f96bbcc9568e1002a3d5a341572d3688e4b9337d
Roman Sergeev <cupper.jj@gmail.com>
2014-01-31 19:21:24 +0400

    remove extra definition

--- src/log/log.cc
+++ src/log/log.cc
@@ -2,9 +2,9 @@
 
 #if BOOST_VERSION < 104600
 # include "log-144.h"
-#elif BOOST_VERSION >= 104600 && BOOST_VERSION < 105400
+#elif BOOST_VERSION < 105400
 # include "log-146.h"
-#elif BOOST_VERSION >= 105400
+#else
 # include "log-154.h"
 #endif
 
