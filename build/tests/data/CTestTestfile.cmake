e9f96a8518b860289c980ae331e8fde4e5b553b2
Nikki Chumakov <nikkikom@gmail.com>
2014-02-24 16:14:52 +0400

    remove build directory from repo

--- build/tests/data/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nikki/Work/github/yamail/tests/data
-# Build directory: /Users/nikki/Work/github/yamail/build/tests/data
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(config)
